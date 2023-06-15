319617900ac8390488ba8b56235a3987f3c0f761
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-15 18:38:21 +0300

    write without stdio

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+* main - entry point
+* Description: Prints "Programming is like building a multilingual puzzle"
+* to the std output
+* Return: 0
+*/
+
+int main(void)
+{
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(STDOUT_FILENO, msg, sizeof(msg) - 1);
+return (1);
+}
