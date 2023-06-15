91417cfc660f87dd85e06946bdf27b33a43ae4d0
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-15 18:06:09 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - entry point
+* Description: Prints "Programming is like building a multilingual puzzle"
+* to the std output
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
