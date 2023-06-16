9196e9ea9d5b4e554c3ca74be3e50ce37e7ef4e4
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-16 15:49:33 +0300

    betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i+1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
