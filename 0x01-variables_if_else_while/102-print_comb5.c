7d155516975e82ed6ee2ae0d11cb70d133627b98
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-16 16:09:14 +0300

    test

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,19 +6,28 @@
 */
 int main(void)
 {
-	int i, j;
+	int i, j, k, l;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
-		if (!(i == 9 && j == 9))
-		{
-			putchar(',');
-			putchar(' ');
-		}
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if (!(i == 9 && j == 9 && k == 9 && l == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
 
