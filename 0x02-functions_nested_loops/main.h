9d442056b9cea902653ff2af31f8fc1620e002f2
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-20 16:38:58 +0300

    absolute val

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include<stdio.h>
 
 int _putchar(char c);
 
@@ -8,5 +9,6 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 
 #endif
