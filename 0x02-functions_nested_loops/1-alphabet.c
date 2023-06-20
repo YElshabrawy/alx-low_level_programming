242b29c3b3a8e12de805b63f18136d2c83fcefbc
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-20 16:04:07 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * print_alphabet - prints alpha in lowercase
+ * Description: prints alpha in lowercase
+ * Return: 0
+*/
+
+void print_alphabet(void)
+{
+	int i;
+	for(i = 0; i < 26; i++)
+		_putchar('a' + i);
+	_putchar('\n');
+}
