369a3a69a415ad6dc8dbbfec6bc69a15cebd54f7
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-20 17:25:41 +0300

    9x9 table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include"main.h"
+/**
+ * times_table - prints 9x9 table
+ * Return: void
+*/
+
+void times_table(void)
+{
+	int r, c;
+
+	for (r = 0;r <= 9; r++)
+	{
+		for (c = 0;c <= 9; c++)
+		{
+			int n = r * c;
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+				if (c != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(n + '0');
+				if (c != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+		}
+			_putchar('\n');
+	}
+}
