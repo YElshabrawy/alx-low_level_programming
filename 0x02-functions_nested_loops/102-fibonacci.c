6cd657bb8ff3098d3764524acc6553444261d04f
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-20 18:30:02 +0300

    fib 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,19 +5,21 @@
  * Return: 0
 */
 
-int main (void)
+int main(void)
 {
-	int n1 = 0 , n2 = 1, c = 2;
+	int n1 = 0, n2 = 1, c = 2;
+
 	printf("%d, ", n1);
 	printf("%d, ", n2);
 	while (c < 50)
 	{
 		int res = n1 + n2;
+
 		printf("%d", res);
 		n1 = n2;
 		n2 = res;
 		c++;
-		if(c != 50 -1)
+		if (c != 50 - 1)
 			printf(", ");
 	}
 	return (0);
