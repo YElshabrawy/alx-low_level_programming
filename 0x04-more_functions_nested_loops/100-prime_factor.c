8ea02393ec857dd7bdfa5fda4146c07bf97e6c94
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-22 20:24:46 +0300

    largest prime fact

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+
+/**
+ * isPrime - checks if a number is prime
+ * @n: num to be checked
+ * return: 1 if is prime
+*/
+int isPrime(unsigned long n)
+{
+	int i;
+
+	if(n == 0 || n == 1)
+			return 0;
+	for (i = 2; i <= sqrt(n); i++)
+		if (n % i == 0)
+			return 0;
+	return 1;
+}
+
+/**
+ * main - entry point
+ * Description return largest prime factor of 612852475143
+ * return: 0
+*/
+int main(void)
+{
+	unsigned long i;
+
+	for (i = 612852475143; i > 0; i--)
+	{
+		if(isPrime(i))
+			break;
+	}
+	printf("%lu", i);
+	return (0);
+}
