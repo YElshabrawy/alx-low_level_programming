e1b818f89df7712aa989e6aa3cb7f14b72c626fe
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-22 20:27:16 +0300

    betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 #include<math.h>
-
 /**
  * isPrime - checks if a number is prime
+ * Description: checks prime
  * @n: num to be checked
- * return: 1 if is prime
+ * Return: 1 if is prime
 */
 int isPrime(unsigned long n)
 {
 	int i;
 
-	if(n == 0 || n == 1)
-			return 0;
+	if (n == 0 || n == 1)
+		return (0);
 	for (i = 2; i <= sqrt(n); i++)
 		if (n % i == 0)
-			return 0;
-	return 1;
+			return (0);
+	return (1);
 }
 
 /**
  * main - entry point
- * Description return largest prime factor of 612852475143
- * return: 0
+ * Description: return largest prime factor of 612852475143
+ * Return: 0
 */
 int main(void)
 {
@@ -29,7 +29,7 @@ int main(void)
 
 	for (i = 612852475143; i > 0; i--)
 	{
-		if(isPrime(i))
+		if (isPrime(i))
 			break;
 	}
 	printf("%lu", i);
