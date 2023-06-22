1a64be4ec0083cec4bf101b1fb94987ae424d884
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-22 20:41:00 +0300

    must be a divisor not jsut the largest prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,37 +1,25 @@
 #include<stdio.h>
 #include<math.h>
 /**
- * isPrime - checks if a number is prime
- * Description: checks prime
- * @n: num to be checked
- * Return: 1 if is prime
-*/
-int isPrime(unsigned long n)
-{
-	int i;
-
-	if (n == 0 || n == 1)
-		return (0);
-	for (i = 2; i <= sqrt(n); i++)
-		if (n % i == 0)
-			return (0);
-	return (1);
-}
-
-/**
  * main - entry point
  * Description: return largest prime factor of 612852475143
  * Return: 0
 */
 int main(void)
 {
-	unsigned long i;
+	unsigned long num = 612852475143;
+	unsigned long  i, largest;
 
-	for (i = 612852475143; i > 0; i--)
-	{
-		if (isPrime(i))
-			break;
-	}
-	printf("%lu\n", i);
+	for (i = 3; i <= sqrt(num); i += 2)
+		while (num % i == 0)
+		{
+			num /= i;
+			largest = i;
+		}
+
+	if (num > 2)
+		largest = num;
+
+	printf("%lu\n", largest);
 	return (0);
 }
