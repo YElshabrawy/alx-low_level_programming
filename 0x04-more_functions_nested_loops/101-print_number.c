90234f188cb5c7272ab2253f545005bfec687b38
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-22 20:53:42 +0300

    solve using recursion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,24 +11,8 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n >= 1000)
-	{
-		_putchar('0' + n / 1000);
-		_putchar('0' + (n / 100) % 10);
-		_putchar('0' + (n / 10) % 10);
-		_putchar('0' + n % 10);
-	}
-	else if (n >= 100)
-	{
-		_putchar('0' + n / 100);
-		_putchar('0' + (n / 10) % 10);
-		_putchar('0' + n % 10);
-	}
-	else if (n >= 10)
-	{
-		_putchar('0' + n / 10);
-		_putchar('0' + n % 10);
-	}
-	else
-		_putchar('0' + n);
+	if ((n / 10) > 0)
+		print_number(n / 10);
+
+	_putchar('0' + n % 10);
 }
