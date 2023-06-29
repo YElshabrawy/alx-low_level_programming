fd473aaba7145c2b7616065e82174d70cf0b6e5d
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-29 12:44:28 +0300

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+* *cap_string - capitalize
+* @s: str
+* Return: cap string
+*/
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i + 1])
+		{
+			char c = s[i];
+
+			int condition = c == ' ' || c == '\t' || c == '\n' ||
+			c == ',' || c == ';' || c == ';' || c == '.' ||
+			c == '!' || c == '?' || c == '"' || c == '(' ||
+			c == ')' || c == '{' || c == '}';
+
+			if (condition && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
+		}
+		i++;
+	}
+
+	return (s);
+}
