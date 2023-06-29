09095b7840f280f294965d39933b9023af279a84
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-29 12:31:28 +0300

    betty 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0')
 	{
-		if(s1[i] != s2[i])
-			return s1[i] - s2[i];
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return 0;
+	return (0);
 }
