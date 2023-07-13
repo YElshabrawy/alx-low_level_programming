66862a576a7a6d4f0eeae1f6bc4496d8cc8b6748
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-13 22:08:21 +0300

    betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,9 +9,9 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-
 	char *res = malloc(nmemb * size);
-	if(!res || nmemb == 0 || size == 0)
+
+	if (!res || nmemb == 0 || size == 0)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
 		res[i] = 0;
