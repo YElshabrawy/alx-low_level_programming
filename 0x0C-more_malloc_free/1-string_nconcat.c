f47fe1bf4183f830f2c8bb90859068cfb654854a
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-13 21:49:31 +0300

    test 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,20 +11,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *res;
-	unsigned int i = 0, len1 = 0, len2 = 0, len;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0, len;
 
-	if (s1)
-		len1 = strlen(s1);
-	if (s2)
-		len2 = strlen(s2);
-	if (n >= len2)
-	{
-		len = len1 + len2;
-		n = len2;
-	}
-	else
-		len = len1 + n;
-	res = malloc((sizeof(char) * len) + 1);
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	n >= len2 ? n = len2 : n;
+
+	res = malloc(sizeof(char) * (len1 + n + 1));
 	if (!res)
 		return (NULL);
 	while (s1[i])
@@ -32,9 +30,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		res[i] = s1[i];
 		i++;
 	}
-	for (i = 0; i < n; i++)
-		res[i + len1] = s2[i];
-	res[i + len1] = '\0';
+
+	for ( ; j < n; j++)
+		res[i + j] = s2[j];
+	res[i + j] = '\0';
 
 	return (res);
 }
