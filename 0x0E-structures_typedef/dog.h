35988c1932b1ccb3b0563949c07297c29d49dc1f
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-18 02:11:41 +0300

    define

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: A struct for a dog with name, age, and owner
+ */
 struct dog
 {
 	char *name;
