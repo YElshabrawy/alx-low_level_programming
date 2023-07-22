483a86b6271ff8134c85b9e0c8136da43c759b18
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-22 17:52:35 +0300

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints num separated by a string
+ * @separator: separator string
+ * @n: num of args
+ * Return: void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	if (n == 0 || !separator)
+		return;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ptr, int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ptr);
+}
