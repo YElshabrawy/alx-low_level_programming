4c89ee21be191fda94461c59835044c2760308bc
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-22 18:32:10 +0300

    7lawa

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,18 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct print - A new struct type defining our print
+ *
+ * @symbol: char representing the data type
+ * @func: func ptr that prints according to symbol
+*/
+typedef struct print
+{
+	char symbol;
+	void (*func)(va_list arg);
+} print_t;
 
 #endif
