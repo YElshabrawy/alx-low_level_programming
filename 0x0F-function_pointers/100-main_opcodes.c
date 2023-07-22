a1201a7ce36d753339116e40d95ab822eaadb901
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-22 13:48:25 +0300

    op code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: params
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	int i, n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		error(1);
+	}
+
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		error(2);
+	}
+
+	n = atoi(argv[1]);
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", ((char *)main)[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
