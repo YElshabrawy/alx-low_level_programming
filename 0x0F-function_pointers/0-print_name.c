8d277dd15288044ddbd70d4d70702d1d0a2fc8c7
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-22 12:26:20 +0300

    AM BACK

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name
+ * @name: string holding name
+ * @f: pointer to a function that takes a name and returns void
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
