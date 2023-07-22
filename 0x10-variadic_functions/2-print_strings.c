51455d4d0087c9c9c59263b06427b10129df28c4
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-07-22 18:03:18 +0300

    jj

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,6 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *res = va_arg(ptr, char*);
+
 		printf("%s", res ? res : "(nil)");
 		if (i != n - 1 && separator)
 			printf("%s", separator);
