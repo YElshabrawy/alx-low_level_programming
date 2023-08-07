5a49a17604445cda8e8d4fd59c78f460b0e6f912
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-08-07 19:06:20 +0300

    append to txt file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: file name
+ * @text_content: string to be appended in file
+ * Return: 1 on success, -1 on fail
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (!fd || (fd && !text_content))
+		return (-1);
+	bytes = write(fd, text_content, strlen(text_content));
+	return (bytes == -1 ? -1 : 1);
+}
