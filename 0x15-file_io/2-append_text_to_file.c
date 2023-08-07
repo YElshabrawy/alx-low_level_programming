2606fde4743a87fe7d9a5e6c91a1a2dbdbae52e5
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-08-07 19:12:30 +0300

    -1 not NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,9 +13,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (!fd || (fd && !text_content))
+	if (fd == -1 || (fd == -1 && !text_content))
 		return (-1);
 	if (text_content)
 		bytes = write(fd, text_content, strlen(text_content));
+	close(fd);
 	return (bytes == -1 ? -1 : 1);
 }
