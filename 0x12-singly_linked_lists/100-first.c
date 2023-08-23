dac3a69dbed684825cc06c481066582b069d1347
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-08-23 17:19:51 +0300

    constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 /**
- * __constructor: constructor
+ * __constructor - runs before main
  * Return: void
 */
 
 void __constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
