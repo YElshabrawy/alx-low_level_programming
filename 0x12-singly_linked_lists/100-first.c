ff51291340157aba1a6084ee6d59a7f204d3fa6e
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-08-23 17:18:00 +0300

    constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * __constructor: constructor
+ * Return: void
+*/
+
+void __constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
