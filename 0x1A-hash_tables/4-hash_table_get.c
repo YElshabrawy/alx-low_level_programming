5e1ac7481e31483b4c9ac309c21cfbbbdf889225
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-04 00:36:27 +0200

    ..

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrives value
+ * @ht: hash table
+ * @key: key
+ * Return: Value
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long idx;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[idx];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return current->value;
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
