700b5a8a5383609d96892859039bd7cd09927c4a
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-11-03 23:35:35 +0200

    ..

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,10 +8,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 	ht->size = size;
+	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t *));
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
 	return (ht);
 }
